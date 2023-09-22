0a7473faece21c01a068d7145d9865a094fa3688
Your Name <09taumang@gmail.com>
2023-09-22 14:18:34 +0200

    minor changes.

--- src/database/users.ts
+++ src/database/users.ts
@@ -3,9 +3,20 @@ import mongoose from "mongoose";
 
 //This is the schema for the login page of the wireframe
 const UserSchema = new mongoose.Schema({
-    username:{type:String, required:true},
+    username:{
+        type:String, 
+        required:true
+    },
     authentication:{
-        password: {type: String, required:true,select:false},
-        salt: {type: String, required:true,select:false},
+        password: {
+            type: String, 
+            required:true,
+            select:false
+        },
+        salt: {
+            type: String, 
+            required:true,
+            select:false
+        },
     }
-}).
+})
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
+import cookieParser from 'cookie-parser';// for handling cookies 
+import compression from 'compression';// for compression is use for HTTP responses.
 import cors from 'cors';
 import mongoose from 'mongoose';
 
 const app = express();
 
+//here the cors middleware is added for credentials
 app.use(cors({
     credentials:true,
 }))
@@ -27,4 +28,5 @@ const MONGO_URL = "mongodb+srv://tau:09Taumang@clusterhrsystem.csqm3gg.mongodb.n
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 
+//index.ts is listening to the errors for the MongoDB connection
 mongoose.connection.on('error',(error:Error) => console.log(error))
