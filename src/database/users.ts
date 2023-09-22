8939a611fb31362c52040240d70e6f911735784d
Your Name <09taumang@gmail.com>
2023-09-22 14:10:20 +0200

    initial commit

---
+++ src/database/users.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+//This is the schema for the login page of the wireframe
+const UserSchema = new mongoose.Schema({
+    username:{type:String, required:true},
+    authentication:{
+        password: {type: String, required:true,select:false},
+        salt: {type: String, required:true,select:false},
+    }
+}).
