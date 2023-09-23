fff6dd2f8a34aa9d791299d14248e3211385cffa
Kagisho Taumang Phahlamohlaka <47403327+taumang@users.noreply.github.com>
2023-09-23 00:45:52 +0000

    added functions

--- src/database/users.ts
+++ src/database/users.ts
@@ -18,5 +18,22 @@ const UserSchema = new mongoose.Schema({
             required:true,
             select:false
         },
+        sessionToken : {
+            type: String,
+            select: false,
+        },
     }
 });
+
+
+// Setting Functions here for interaction with the database. 
+export const UserModel = mongoose.model('User', UserSchema);
+
+export const getUsers = () => UserModel.find();// purpose to get data from the database.
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken':sessionToken,
+});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values:Record<string, any>) => new UserModel(values).save().then((user)=> user.toObject());
+export const deleteUserId = (id:string) => UserModel.findOneAndDelete({_id: id});
+export const updateUserById = (id:string, values:Record<string,any>) => UserModel.findByIdAndUpdate(id,values);
